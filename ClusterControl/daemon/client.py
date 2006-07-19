76a8958bf4a457b25b85dc46b17f248b0d03739d
aronb <aronb@d6109375-3c62-66dd-b723-c7798c6675f1>
2006-07-19 21:39:15 +0000

    - Remove old code.

--- ClusterControl/daemon/client.py
+++
@@ -1,6 +0,0 @@
-import Pyro.core
-
-# you have to change the URI below to match your own host/port.
-#cs = Pyro.core.getProxyForURI("PYROLOC://localhost:7766/cluster_server")
-cs = Pyro.core.getProxyForURI("PYROLOC://infiscape3:7766/cluster_server")
-cs.test()
