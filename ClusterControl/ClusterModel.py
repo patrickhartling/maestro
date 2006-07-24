ecf8d3fcac5d83881a7ca77588530e391d035347
aronb <aronb@d6109375-3c62-66dd-b723-c7798c6675f1>
2006-07-24 21:58:03 +0000

    - Pyro proxies are not in the ClusterModel anymore.

--- ClusterControl/ClusterModel.py
+++ ClusterControl/ClusterModel.py
@@ -23,6 +23,16 @@ HPUX = 6
 AIX = 7
 SOLARIS = 8
 
+OsNameMap = {ERROR  : 'Error',
+             LINUX  : 'Linux',
+             WIN    : 'Windows',
+             WINXP  : 'Windows XP',
+             MACOS  : 'MacOS',
+             MACOSX : 'MacOS X',
+             HPUX   : 'HP UX',
+             AIX    : 'AIX',
+             SOLARIS : 'Solaris'}
+
 class ClusterModel(QtCore.QAbstractListModel):
    def __init__(self, xmlTree, parent=None):
       QtCore.QAbstractListModel.__init__(self, parent)
@@ -66,8 +76,27 @@ class ClusterModel(QtCore.QAbstractListModel):
       def debugCallback(message):
          sys.stdout.write("DEBUG: " + message)
 
-   def init(self, eventDispatcher):
+   def init(self, eventManager, eventDispatcher):
+      self.mEventManager = eventManager
       self.mEventDispatcher = eventDispatcher
+      self.mEventManager.connect("*", "settings.os", self.onReportOs)
+
+   def onReportOs(self, nodeId, os):
+      try:
+         print "onReportOs [%s] [%s]" % (nodeId, os)
+         changed = False
+         for node in self.mNodes:
+            if node.getIpAddress() == nodeId:
+               if os != node.mPlatform:
+                  node.mPlatform = os
+                  changed = True
+
+         if changed:
+            # TODO: Only send changed signal for nodes really changed.
+            self.emit(QtCore.SIGNAL("dataChanged(QModelIndex,QModelIndex)"), QtCore.QModelIndex(), QtCore.QModelIndex())
+            #self.emit(QtCore.SIGNAL("dataChanged(int)"), 0)
+      except Exception, ex:
+         print "ERROR: ", ex
 
    def insertRows(self, row, count, parent):
       self.beginInsertRows(QtCore.QModelIndex(), row, row + count - 1)
@@ -112,10 +141,19 @@ class ClusterModel(QtCore.QAbstractListModel):
 
       new_connections = False
 
-      for n in self.mNodes:
-         if (not self.mEventDispatcher.isConnected(n.getIpAddress())):
-            if self.mEventDispatcher.connect(n.getIpAddress()):
-               new_connections = True
+      # Iterate over nodes and try to connect to nodes that are not connected.
+      for node in self.mNodes:
+         try:
+            # Attempt to get the IP address from the hostname.
+            ip_address = node.getIpAddress()
+            # If node is not connected, attempt to connect.
+            if not self.mEventDispatcher.isConnected(ip_address):
+               if self.mEventDispatcher.connect(ip_address):
+                  new_connections = True
+                  # Tell the new node to report it's os.
+                  self.mEventDispatcher.emit(ip_address, "settings.get_os", ())
+         except Exception, ex:
+            print "WARNING: Could not connect to [%s]" % (node.getHostname())
 
       if new_connections:
          print "We had new connections"
@@ -131,55 +169,58 @@ class ClusterModel(QtCore.QAbstractListModel):
          n.stopCommand()
 
    def data(self, index, role=QtCore.Qt.DisplayRole):
+      """ Returns the data representation of each node in the cluster.
+      """
       if not index.isValid():
          return QtCore.QVariant()
-        
+
+      # Get the cluster node we want data for.
+      cluster_node = self.mNodes[index.row()]
+
+      # Return an icon representing the operating system.
       if role == QtCore.Qt.DecorationRole:
-         cluster_node = self.mNodes[index.row()]
-         #try:
-         #   index = cluster_node.proxy().getService("Settings").getPlatform()
-         #   return QtCore.QVariant(self.mIcons[index])
-         #except:
-         return QtCore.QVariant(self.mIcons[ERROR])
+         return QtCore.QVariant(self.mIcons[cluster_node.mPlatform])
+      # Return the name of the node.
       elif role == QtCore.Qt.DisplayRole or role == QtCore.Qt.EditRole:
-         return QtCore.QVariant(str(self.mNodes[index.row()].getName()))
+         return QtCore.QVariant(str(cluster_node.getName()))
       elif role == QtCore.Qt.UserRole:
-         return self.mNodes[index.row()]
+         return cluster_node
        
       return QtCore.QVariant()
 
    def rowCount(self, parent=QtCore.QModelIndex()):
+      """ Returns the number of nodes in the current cluster configuration.
+      """
+      # If the parent is not valid, then we have no children.
       if parent.isValid():
          return 0
       else:
          return len(self.mNodes)
 
    def setData(self, index, value, role):
+      """ Doesn't do anything but provide a way to fire a dataChanged event
+          for a given model index.
+      """
       self.emit(QtCore.SIGNAL("dataChanged(QModelIndex,QModelIndex)"), index, index)
       self.emit(QtCore.SIGNAL("dataChanged(int)"), index.row())
       return True
-         
+
 class ClusterNode:
+   """ Represents a node in the active cluster configuration. Most of this
+       information is loaded from the configuration file. But things like
+       the current OS are retrieved from the remote object.
+   """
    def __init__(self, xmlElt):
       assert xmlElt.tag == "cluster_node"
       self.mElement = xmlElt
       #print "Name:", self.mElement.get("name")
       #print "HostName:", self.mElement.get("hostname")
-      self.mProxy = None
       self.mName = self.mElement.get("name")
       self.mHostname = self.mElement.get("hostname")
       self.mClass = self.mElement.get("sub_class")
       self.mOutputThread = None
       self.mRunningCommand = ""
-
-   def getClass(self):
-      if not None == self.mProxy:
-         try:
-            platform = self.mProxy.getService("Settings").getPlatformName()
-            return platform + "," + self.mClass
-         except:
-            pass
-      return self.mClass
+      self.mPlatform = ERROR 
 
    def getName(self):
       return self.mElement.get("name")
@@ -188,30 +229,23 @@ class ClusterNode:
       return self.mElement.set("name", newName)
 
    def getHostname(self):
-      return self.mHostname
+      return self.mElement.get("hostname")
 
    def setHostname(self, newHostname):
+      self.mPlatform = ERROR
       return self.mElement.set("hostname", newHostname)
 
    def getIpAddress(self):
-      return socket.gethostbyname(self.mHostname)
+      return socket.gethostbyname(self.getHostname())
 
-   def connect(self):
-      if None == self.mProxy:
-         try:
-            print "Trying to connect to: PYROLOC://%s:7766/cluster_server" % (self.getHostname())
-            self.mProxy = Pyro.core.getProxyForURI("PYROLOC://" + self.getHostname() + ":7766/cluster_server")
-            print "Connected to [%s] [%s]" % (self.getName(), self.mProxy.GUID())
-            return True
-         except:
-            self.mProxy = None
-            print "Error connecting proxy to [%s]" % (self.getHostname())
-      else:
-         print "Cluster node [%s] already has an active proxy." % (self.getName())
-      return False
+   def getPlatformName(self):
+      return OsNameMap[self.mPlatform]
 
-   def proxy(self):
-      return self.mProxy
+   def getClass(self):
+      platform = self.getPlatformName()
+      if platform > 0:
+         return platform + "," + self.mClass
+      return self.mClass
 
    def isOutputThreadRunning(self):
       if not None == self.mOutputThread:
@@ -241,11 +275,6 @@ class ClusterNode:
          self.mProxy.stopCommand()
       else:
          print "Cluster node [%s] is not connected." % (self.getName())
-
-   def disconnect(self):
-      if not None == self.mProxy:
-         del self.mProxy
-         self.mProxy = None
 
 class OutputThread(threading.Thread):
    def __init__(self, proxy, node, outputLogger):
