224f034bb7cc2eae38296009533992cafa22913c
aronb <aronb@d6109375-3c62-66dd-b723-c7798c6675f1>
2006-07-24 16:54:44 +0000

    - Ensure that emiting events is non-blocking.

--- ClusterControl/util/EventDispatcher.py
+++ ClusterControl/util/EventDispatcher.py
@@ -15,11 +15,13 @@ class EventDispatcher(object):
       self.mCallback = callback
 
    def register(self, nodeId, obj):
-      """ Register object to recieve callback events for the given node."""
+      """ Register object to recieve callback events for the given node.
+      """
       if self.mConnections.has_key(nodeId):
          raise AttributeError("EventDispatcher.connect: already connected to [%s]" % (nodeId))
 
       self.mConnections[nodeId] = obj
+      obj._setOneway(['emit'])
 
    def connect(self, nodeId):
       """ Connect to the given nodes event manager.
@@ -37,6 +39,7 @@ class EventDispatcher(object):
          proxy = Pyro.core.getProxyForURI("PYROLOC://" + nodeId + ":7766/cluster_server")
          print "Connected to [%s] [%s]" % (nodeId, proxy.GUID())
          self.mConnections[nodeId] = proxy
+         proxy._setOneway(['register', 'emit'])
          proxy.register(self.mHostname, self.mCallback)
       except Exception, ex:
          print "Error connecting proxy to [%s]" % (nodeId)
@@ -69,12 +72,14 @@ class EventDispatcher(object):
       # Get local IP address to use for nodeId mask on remote nodes.
       ip_address = Pyro.protocol.getIPAddress(self.mHostname)
 
+      # Build up a list of all connections to emit signal on.
       node = []
       if nodeId == "*":
          nodes = self.mConnections.items()
       elif self.mConnections.has_key(nodeId):
          nodes = [(node, self.mConnections[nodeId])]
 
+      # Emit signal to selected nodes, removing any that have dropped their connection.
       for k, v in nodes:
          try:
             v.emit(ip_address, sigName, argsTuple)
